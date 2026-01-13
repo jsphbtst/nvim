cebfec9af9e31aefebf349a10219d571d0f5ca13
Joseph Bautista <joseph@noloco.io>
2026-01-13 12:26:44 +0100

    chore: add winbar

--- init.lua
+++ init.lua
@@ -42,10 +42,16 @@ require("lazy").setup({
         sections = {
           lualine_a = { "mode" },
           lualine_b = { "branch", "diff", "diagnostics" },
-          lualine_c = { { "filename", path = 1 } },
+          lualine_c = {},
           lualine_x = { "encoding", "fileformat", "filetype" },
           lualine_y = { "progress" },
           lualine_z = { "location" },
+        },
+        winbar = {
+          lualine_c = { { "filename", path = 1 } },
+        },
+        inactive_winbar = {
+          lualine_c = { { "filename", path = 1 } },
         },
       })
     end,
